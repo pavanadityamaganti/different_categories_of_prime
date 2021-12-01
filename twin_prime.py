efe52ab56d4bc5512f88f6892a93dbb2a6d8380e
Pavan Aditya <84063768+pavanadityamaganti@users.noreply.github.com>
2021-12-01 17:55:26 +0530

    Add files via upload

---
+++ twin_prime.py
@@ -0,0 +1,18 @@
+from math import *
+def isprime(num):
+    if num==1:
+        return False
+    sq=int(sqrt(num))
+    for i in range(2,sq+1):
+        if num%i==0:
+            return False
+    return True
+
+a,b=map(int,input().split())
+c=0
+while a<b:
+    if a%2 and isprime(a) and isprime(a+2):
+        c+=1
+        a+=3
+    a+=1
+print(c)
